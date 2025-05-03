f20b21f48405b486ffe476aed56d278b29f61c1b
vincent-copr <vincentwijaya2001@yahoo.com>
2025-05-03 20:58:08 +1000

    Add input validation and error display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { DEFICIT, MAX_ACTIVITY_LEVEL, MIN_ACTIVITY_LEVEL } from "./constants";
+import { DEFICIT, MAX_ACTIVITY_LEVEL, MIN_ACTIVITY_LEVEL, MIN_AGE } from "./constants";
 import "./App.css";
 import { useState } from "react";
 import { FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material";
@@ -9,21 +9,40 @@ function App() {
   const [sex, setSex] = useState<Sex>();
   const [age, setAge] = useState<number>();
   const [weight, setWeight] = useState<number>();
-  const [activityLevelMultiplier, setActivityLevelMultiplier] = useState<number>();
+  const [activityLevel, setActivityLevel] = useState<number>();
   const [bmr, setBmr] = useState<number>(0);
   const [energyRequirement, setEnergyRequirement] = useState<number>(0);
   const [energyRequirementSubtracted, setEnergyRequirementSubtracted] = useState<number>(0);
+  const [errors, setErrors] = useState<string[]>([]);
 
-  const handleSubmit = () => {
-    if (!sex || !age || !weight || !activityLevelMultiplier) {
-      alert("Please fill in all fields.");
-      return;
+  const validate = (): boolean => {
+    const currentErrors: string[] = [];
+
+    if (!sex || !age || !weight || !activityLevel) {
+      currentErrors.push('Missing fields');
+      setErrors(currentErrors);
+      return false;
     }
 
-    const bmr = calculateBMR(age, weight, sex);
+    if (age < 18) {
+      currentErrors.push('Age must be above 18');
+    }
+
+    if (activityLevel < MIN_ACTIVITY_LEVEL || activityLevel > MAX_ACTIVITY_LEVEL) {
+      currentErrors.push(`Activity Level must be between ${MIN_ACTIVITY_LEVEL} and ${MAX_ACTIVITY_LEVEL}`);
+    }
+
+    setErrors(currentErrors);
+    return errors.length === 0;
+  }
+
+  const handleSubmit = () => {
+    if (!validate()) return;
+
+    const bmr = calculateBMR(age!, weight!, sex!);
     setBmr(Math.round(bmr));
 
-    const energyRequirement = calculateEnergyRequirement(bmr, activityLevelMultiplier);
+    const energyRequirement = calculateEnergyRequirement(bmr, activityLevel!);
     setEnergyRequirement(Math.round(energyRequirement));
 
     const energyRequirementSubtracted = calculateEER(energyRequirement);
@@ -42,7 +61,7 @@ function App() {
         <h1 className="font-bold text-black mb-6 text-center">
           Energy Deficit Calculator
         </h1>
-        <FormControl className="space-y-2 text-black">
+        <FormControl className="space-y-1 text-black">
           <RadioGroup
             value={sex}
             row
@@ -66,33 +85,32 @@ function App() {
               type="number"
               id="age"
               name="age"
+              min={MIN_AGE}
               required
               className="w-full px-4 py-1 text-black rounded-md border border-gray-300 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your age"
+              placeholder="Enter the age"
               onChange={(e) => setAge(Number(e.target.value))}
             />
           </div>
 
-          <div className="flex items-center gap-1">
-            <div className="space-y-1 flex-1">
-              <label
-                htmlFor="weight"
-                className="block text-sm font-medium text-black"
-              >
-                Adjusted Body Weight (kg):
-              </label>
-              <input
-                value={weight}
-                type="number"
-                id="weight"
-                name="weight"
-                color=""
-                required
-                className="w-full text-black px-4 py-1 rounded-md border border-gray-300 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                placeholder="Enter your weight"
-                onChange={(e) => setWeight(Number(e.target.value))}
-              />
-            </div>
+          <div className="space-y-1">
+            <label
+              htmlFor="weight"
+              className="block text-sm font-medium text-black"
+            >
+              Adjusted Body Weight (kg):
+            </label>
+            <input
+              value={weight}
+              type="number"
+              id="weight"
+              name="weight"
+              color=""
+              required
+              className="w-full text-black px-4 py-1 rounded-md border border-gray-300 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              placeholder="Enter the weight"
+              onChange={(e) => setWeight(Number(e.target.value))}
+            />
           </div>
 
           <div className="space-y-1">
@@ -103,7 +121,7 @@ function App() {
               Activity Level:
             </label>
             <input
-              value={activityLevelMultiplier}
+              value={activityLevel}
               type="number"
               id="activity-level"
               name="weight"
@@ -112,18 +130,30 @@ function App() {
               max={MAX_ACTIVITY_LEVEL}
               required
               className="w-full text-black px-4 py-1 rounded-md border border-gray-300 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              placeholder="Enter your weight"
-              onChange={(e) => setActivityLevelMultiplier(Number(e.target.value))}
+              placeholder="Enter the activity level"
+              onChange={(e) => setActivityLevel(Number(e.target.value))}
             />
           </div>
 
           <button
             type="submit"
             onClick={handleSubmit}
-            className="w-full bg-blue-500 hover:bg-blue-600 text-black font-semibold py-2.5 rounded-md transition-colors duration-300"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg my-2 py-2.5 rounded-md transition-colors duration-300"
           >
             Calculate
           </button>
+
+          <div>
+            {errors.length > 0 && (
+              <div className="text-red-500">
+                {
+                  errors.map((error, index) => (
+                    <p key={index}>{error}</p>
+                  ))
+                }
+              </div>
+            )}
+          </div>
         </FormControl>
         {
           bmr && energyRequirement && energyRequirementSubtracted && (
